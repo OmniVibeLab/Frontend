da23f5c06fdbecb32a47ca036dd529f736182dc2
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-09-05 21:37:36 +0545

    add

--- server/config/env.config.ts
+++
@@ -1,3 +0,0 @@
-import { config } from "dotenv"
-config()
-export const { PORT, DB_URI } = process.env
--- server/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./app";
-import { connectToDB } from "./config/dbConnection.config";
-import { PORT } from "./config/env.config";
-
-app.listen(PORT, () => {
-    connectToDB().then(() => {
-        console.log(`Serving on http://localhost:${PORT}`);
-    })
-})
